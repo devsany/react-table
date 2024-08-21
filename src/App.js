aab1ea6d69390141a8e795139528bd04143a5857
devsany <sunnyrajbodhgaya19@gmail.com>
2024-08-21 10:15:55 +0530

    react-table first commit add json file , add BasicTable.jsx file , add column.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import BasicTable from "./component/BasicTable";
 
 function App() {
   return (
     <div className="App">
+      <BasicTable />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
